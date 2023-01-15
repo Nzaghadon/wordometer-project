ba87e2feee27550a200e27e152b863930b8ee21f
Nzagha Ikechukwu <nzaghaikechukwu@gmail.com>
2023-01-15 12:55:42 +0100

    The word count function

--- firstfiles/wordometer.js
+++ firstfiles/wordometer.js
@@ -1,1 +1,18 @@
 // JavaScript source code
+const words = document.getElementById('input');
+const count = document.getElementById('count');
+
+
+words.addEventListener('input', function countWord() {
+    let res = [];
+    let str = this.value.replace(/[\t\n\r\.\?\!]/gm, " ").split(" ");
+    str.map((s) => {
+        let trimStr = s.trim();
+        if (trimStr.length > 0) {
+            res.push(trimStr);
+        }
+    })
+    count.innerText = res.length;
+})
+
+
